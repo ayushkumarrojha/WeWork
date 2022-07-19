0aa43d52f8a386936da28983bd0ef0f98ef08779
varshar <varshar@geekyants.com>
2022-07-19 09:28:25 +0530

    Updated scripts with assertions- July 19

--- src/test/java/sanity/SelectSubscriptionsTest.java
+++ src/test/java/sanity/SelectSubscriptionsTest.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.*;
 @Listeners(utilities.Listeners.class)
 public class SelectSubscriptionsTest extends CommonOps 
 {
-	public static org.apache.logging.log4j.Logger log = LogManager.getLogger(SelectSubscriptionsTest.class.getName());
+	//public static org.apache.logging.log4j.Logger log = LogManager.getLogger(SelectSubscriptionsTest.class.getName());
 	
 	@Test(description = "Test01 - Verify User is able to select subscriptions" ,dataProvider = "Subscriptions")
     @Description("This test verifies that User is able to select subscriptions")
@@ -28,9 +28,9 @@ public class SelectSubscriptionsTest extends CommonOps
     {
 		WebFlows.loadWebsite();
     	WebFlows.loginToApplication();
-    	log.info("Logged in to the application");
+    	//log.info("Logged in to the application");
     	click(webLoading.workspaceDrpdwn);
-    	log.info("Clicked on workspace dropdown");
+    	//log.info("Clicked on workspace dropdown");
     	click(webLoading.opt(officeType));
     	String subscriptionType = webLoading.subscriptionType.getText();
     	if(subscriptionType.contains(textInHeader))
@@ -41,27 +41,27 @@ public class SelectSubscriptionsTest extends CommonOps
         	click(webLoading.select("Blue One Square"));
         	Verifications.elementIsVisible(webLoading.select(selectOption));
         	click(webLoading.select(selectOption));
-        	log.info("Selected location as Delhi - Blue One square");
+        	//log.info("Selected location as Delhi - Blue One square");
         	Verifications.elementIsVisible(webLoading.nameField);
         	updateText(webLoading.nameField, getData("name"));
             updateText(webLoading.emailField, getData("email"));
             updateText(webLoading.phonenNumberField, getData("phoneNumber"));
-            log.info("Entered username , email and phone number details");
+           // log.info("Entered username , email and phone number details");
             scrollToElement(webLoading.plusIcon);
 			UIActions.selectNoOfPeople(2);
 			UIActions.selectDate("2024", "September", "26");
-			log.info("Selected number of people and date");
+			//log.info("Selected number of people and date");
             click(webLoading.continueBtn);
             String actualText = webLoading.thankyouText.getText();
             Verifications.verifyText(actualText,"Thank you for contacting WeWork");
-            log.info("Form submission successful");
+           // log.info("Form submission successful");
             click(webLoading.backToHomePageBtn);
             WebFlows.logoutOfApplication();
-            log.info("Logged out of the application");
+          //  log.info("Logged out of the application");
     	}
     	else
     	{
-    		log.error("Subscription type mismatched in the header");
+    		//log.error("Subscription type mismatched in the header");
     		Assert.assertTrue(false, "Subscription type mismatch");
     		
     	}
